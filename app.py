a0bf3b5b8e24961ad36582d4092530f1253922ea
Team <yehudarothstein@gmail.com>
2024-02-15 22:13:02 +0200

    init commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,7 +9,7 @@ def hello_world():
 
 @app.route('/hi')
 def hi():
-    return 'Hello'
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=true)
